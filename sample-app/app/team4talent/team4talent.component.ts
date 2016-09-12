cb161ca41ede7c2cd43b863e26f664940b1b4f26
Joris Brauns <Joris Brauns>
2016-09-12 16:50:42 +0200

    remove old page

--- sample-app/app/team4talent/team4talent.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './app/team4talent/team4talent.component.html',
-    styleUrls: ['./app/team4talent/team4talent.component.css']
-})
-export class Team4TalentComponent {
-
-}
